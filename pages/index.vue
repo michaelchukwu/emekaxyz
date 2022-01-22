51a847604e60badd645bfe6893d33aed4742ca00
Michael Chukwu <naemugo@gmail.com>
2022-01-23 00:54:46 +0100

    cruise night

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <Tutorial/>
+  <div>
+    Welcome to my site, I want to confess that it can be finer than this. But I
+    don't have time. If you need me, please send a mail to naemugo@gmail.com.
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
